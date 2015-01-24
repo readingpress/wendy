975cb4cc36853adc5b44b605975140f69239a2ee
michaeldewolf85 <michaeldewolf85@gmail.com>
2015-01-24 12:08:46 -0500

    Add application logic to app.

---
+++ docroot/app/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+/**
+ * @file
+ * Bootstrap the application.
+ */
--- docroot/index.php
+++ docroot/index.php
@@ -1,3 +1,18 @@
+<?php
+
+/**
+ * @file
+ * The PHP page that serves all page requests on a Wendy installation. The 
+ * routines here dispatch control to the appropriate handler.
+ */
+
+/**
+ * Root directory of the Wendy installation.
+ */
+define('WENDY_ROOT', getcwd());
+require_once WENDY_ROOT . '/app/bootstrap.php';
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
