8772566a2c32637bba92d2b7f78c31d923ea34da
michaeldewolf85 <michaeldewolf85@gmail.com>
2015-01-24 14:02:33 -0500

    Add Welles Wilder class and interface.

--- docroot/app/bootstrap.php
+++ docroot/app/bootstrap.php
@@ -7,3 +7,5 @@
 
 require_once WENDY_ROOT . '/app/model/csvdata/CSVDataInterface.inc';
 require_once WENDY_ROOT . '/app/model/csvdata/CSVData.inc';
+require_once WENDY_ROOT . '/app/model/welleswilder/WellesWilderInterface.inc';
+require_once WENDY_ROOT . '/app/model/welleswilder/WellesWilder.inc';
