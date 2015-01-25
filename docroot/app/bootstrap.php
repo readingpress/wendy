bdcefd379764c312da72770e0788edb84ad88f04
michaeldewolf85 <michaeldewolf85@gmail.com>
2015-01-24 19:08:50 -0500

    Output a report of USD pairs.

--- docroot/app/bootstrap.php
+++ docroot/app/bootstrap.php
@@ -7,5 +7,8 @@
 
 require_once WENDY_ROOT . '/app/model/csvdata/CSVDataInterface.inc';
 require_once WENDY_ROOT . '/app/model/csvdata/CSVData.inc';
+require_once WENDY_ROOT . '/app/model/trade/TradeInterface.inc';
+require_once WENDY_ROOT . '/app/model/trade/Trade.inc';
 require_once WENDY_ROOT . '/app/model/welleswilder/WellesWilderInterface.inc';
 require_once WENDY_ROOT . '/app/model/welleswilder/WellesWilder.inc';
+require_once WENDY_ROOT . '/app/model/welleswilder/ParabolicTimePriceSystem.inc';
--- docroot/index.php
+++ docroot/index.php
@@ -6,12 +6,91 @@
  * routines here dispatch control to the appropriate handler.
  */
 
+error_reporting(-1);
+ini_set('display_errors', 'On');
+
 /**
  * Root directory of the Wendy installation.
  */
 define('WENDY_ROOT', getcwd());
 require_once WENDY_ROOT . '/app/bootstrap.php';
 
+print '<h3>AUD/USD</h3>';
 $data = new CSVData(WENDY_ROOT . '/data/AUDUSD_day.csv');
-print '<pre>';
-$ww = new WellesWilder($data->getCSVData());
+$ww = new ParabolicTimePriceSystem($data->getCSVData());
+$ww->crunch();
+$trades = $ww->getTrades();
+$profit = 0;
+$winners = 0;
+foreach ($trades as $trade) {
+	if ($trade->winner()) {
+		$winners++;
+	}
+	$profit += $trade->getProfit();
+}
+print 'Profit: $' . round($profit, 2) . '<br />';
+print 'Success Rate: ' . round($winners / count($trades) * 100) . '%<br />';
+
+print '<h3>EUR/USD</h3>';
+$data = new CSVData(WENDY_ROOT . '/data/EURUSD_day.csv');
+$ww = new ParabolicTimePriceSystem($data->getCSVData());
+$ww->crunch();
+$trades = $ww->getTrades();
+$profit = 0;
+$winners = 0;
+foreach ($trades as $trade) {
+	if ($trade->winner()) {
+		$winners++;
+	}
+	$profit += $trade->getProfit();
+}
+print 'Profit: $' . round($profit, 2) . '<br />';
+print 'Success Rate: ' . round($winners / count($trades) * 100) . '%<br />';
+
+print '<h3>NZD/USD</h3>';
+$data = new CSVData(WENDY_ROOT . '/data/NZDUSD_day.csv');
+$ww = new ParabolicTimePriceSystem($data->getCSVData());
+$ww->crunch();
+$trades = $ww->getTrades();
+$profit = 0;
+$winners = 0;
+foreach ($trades as $trade) {
+	if ($trade->winner()) {
+		$winners++;
+	}
+	$profit += $trade->getProfit();
+}
+print 'Profit: $' . round($profit, 2) . '<br />';
+print 'Success Rate: ' . round($winners / count($trades) * 100) . '%<br />';
+
+print '<h3>USD/CAD</h3>';
+$data = new CSVData(WENDY_ROOT . '/data/USDCAD_day.csv');
+$ww = new ParabolicTimePriceSystem($data->getCSVData());
+$ww->crunch();
+$trades = $ww->getTrades();
+$profit = 0;
+$winners = 0;
+foreach ($trades as $trade) {
+	if ($trade->winner()) {
+		$winners++;
+	}
+	$profit += $trade->getProfit();
+}
+print 'Profit: $' . round($profit, 2) . '<br />';
+print 'Success Rate: ' . round($winners / count($trades) * 100) . '%<br />';
+
+print '<h3>USD/CAD</h3>';
+$data = new CSVData(WENDY_ROOT . '/data/USDCHF_day.csv');
+$ww = new ParabolicTimePriceSystem($data->getCSVData());
+$ww->crunch();
+$trades = $ww->getTrades();
+$profit = 0;
+$winners = 0;
+foreach ($trades as $trade) {
+	if ($trade->winner()) {
+		$winners++;
+	}
+	$profit += $trade->getProfit();
+}
+print 'Profit: $' . round($profit, 2) . '<br />';
+print 'Success Rate: ' . round($winners / count($trades) * 100) . '%<br />';
