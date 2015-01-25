006672efb60ce4b38fdf2dfe439d46717540f717
michaeldewolf85 <michaeldewolf85@gmail.com>
2015-01-25 17:56:08 -0500

    Add directional movement class structure.

--- docroot/index.php
+++ docroot/index.php
@@ -15,38 +15,3 @@ ini_set('display_errors', 'On');
 define('WENDY_ROOT', getcwd());
 require_once WENDY_ROOT . '/app/bootstrap.php';
 
-print '<h3>AUD/USD</h3>';
-$data = new CSVData(WENDY_ROOT . '/data/AUDUSD_day.csv');
-$ww = new ParabolicTimePriceSystem($data->getCSVData());
-$ww->crunch();
-$trades = $ww->getTrades();
-$profit = 0;
-$winners = 0;
-$largest = 0;
-$max = 0;
-foreach ($trades as $trade) {
-	if ($trade->winner()) {
-		$winners++;
-	}
-	$p = $trade->getProfit();
-	$largest = $p > $largest ? $p : $largest;
-	$profit += $p;
-	$max += $trade->getMax();
-}
-print 'Total Trades: ' . count($trades) . '<br />';
-print 'Total Profit: $' . number_format($profit, 4) . '<br />';
-print 'Largest Profit: $' . number_format($largest, 4) . '</br>';
-print 'Average Profit: $' . number_format($profit / count($trades), 4) . '</br>';
-print 'Total Highs: $' . number_format($max, 4) . '<br />';
-print 'Average High: $' . number_format($max / count($trades), 4) . '<br />';
-print 'Success Rate: ' . number_format($winners / count($trades) * 100) . '%<br />';
-
-$account = 2000;
-foreach ($trades as $trade) {
-	$chunk = $account / 10;
-	$profit = $chunk * number_format($trade->getProfit() - .0004, 4) * 50;
-	$account += $profit;
-}
-
-print 'Account starting at $2000 trading 10% on each trade, with 50 leverage.<br />';
-print '$' . number_format($account, 2);
